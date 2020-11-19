2904724b05b58be8e2a64940f4f332fb0546e0f7
Jacob Counts <infamous213@gmail.com>
2020-11-18 19:22:25 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# weatherDashboard
+# Weather Dashboard
+
+## Operation 
+
+Enter city you would like to check the weather for. Previous searches will be saved
+to make it easier to search your favorite cities.
+
--- apps.js
+++ apps.js
@@ -6,14 +6,14 @@ var date =
   today.getMonth() + 1 + "-" + today.getDate() + "-" + today.getFullYear();
 
 const apikey = "a45c2483a3fc4fe6b57abb8e9140f64b";
-
-getWeatherDetails("Phoenix");
+createButton();
+//getWeatherDetails("Phoenix");
 
 function getWeatherDetails(city) {
   const query =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    "&APPID=" +
+    "&appid=" +
     apikey;
 
   $.ajax({
@@ -31,21 +31,36 @@ function getWeatherDetails(city) {
     //console.log(weather);
     getUVindex(weather.coord.lat, weather.coord.lon);
     fiveDay(city);
-    createButton(city);
+    setLocalStorage(city);
   });
 }
-function createButton(city) {
-  // var listItem = document.createElement("li")
-  var listItem = $("<li>")
-    .addClass("list-group-item list-group-item-action")
-    .text(city);
-  $("#buttons-view").append(listItem);
+function setLocalStorage(city) {
+  var cities = JSON.parse(localStorage.getItem("city")) || []
+  cities.push(city);
+  var uniques = [...new Set(cities)]
+  localStorage.setItem("city", JSON.stringify(uniques));
+  createButton()
 }
+function createButton() {
+  var cities = JSON.parse(localStorage.getItem("city")) || []
+  $("#buttons-view").html("");
+  for (let i = 0; i < cities.length; i++) {
+    
+    var listItem = $("<li>")
+      .addClass("list-group-item list-group-item-action")
+      .text(cities[i])
+      .click(()=>getWeatherDetails(cities[i]))
+    $("#buttons-view").append(listItem);
+    
+  }
+}
+
 
 function kelvinToF(k) {
   return ((k - 273.15) * 1.8 + 32).toFixed(2);
 }
 
+// UV Index information 
 var getUVindex = function (lat, lon) {
   const query2 =
     "https://api.openweathermap.org/data/2.5/uvi?appid=" +
@@ -54,19 +69,29 @@ var getUVindex = function (lat, lon) {
     lat +
     "&lon=" +
     lon;
-  // "&appid=" +
-  // apikey;
 
   fetch(query2)
     .then((res) => res.json())
     .then((uvindex) => {
       console.log(uvindex);
-      var btnUV = $("<span>").text(uvindex);
+      var btnUV = $("<span>").text(uvindex.value).addClass("btn btn-sm");
+      if (uvindex.value < 3) {
+        btnUV.addClass("btn-success")
+        
+      }else if (uvindex.value < 6) {
+        btnUV.addClass("btn-warning")
+
+        
+      }else {
+        btnUV.addClass("btn-danger")
+      }
+
       $(".uv-index").text("UV Index: ");
       $(".uv-index").append(btnUV);
     });
 };
 
+// Five Day forcast 
 function fiveDay(city) {
   const query3 =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -86,9 +111,10 @@ function fiveDay(city) {
       for (let i = 0; i < 40; i++) {
         var option = data.list[i].dt_txt.substring(11);
         var dateValue = data.list[i].dt_txt.substring(0, 10);
-        var currentDate = date;
+        var currentDate = date;//change to date from data
         if ("15:00:00" == option && dateValue != currentDate) {
           iconArray.push(data.list[i].weather[0].icon);
+          dateArray.push(dateValue);
           console.log(iconArray);
           tempArray
             .push((data.list[i].main.temp - 273.15) * 1.8 + 32)
@@ -97,24 +123,27 @@ function fiveDay(city) {
         }
       }
 
-      //$(".uv-index").text("UV Index: ");
 
       for (let i = 0; i < dateArray.length; i++) {
+        console.log("hello");
         var newDate = $("<h4>").text(dateArray[i]);
-        $(".forecast" + [i]).append(newDate);
+        $(`.forecast${i}`).append(newDate);
 
         var newImg = $("<img>");
         newImg.attr(
           "src",
-          "https://api.openweathermap.org/img/wn/" + iconArray[i] + "@2x.png"
+          "https://api.openweathermap.org/img/w/" + iconArray[i] + ".png"
         );
-        $(".forecast" + [i]).append(newImg);
+        $(".forecast" + i).append(newImg);
 
-        var newTemp = $("<p>").html("Temp: " + tempArray[i] + "&#176;F");
-        $(".forecast" + [i]).append(newTemp);
+        var newTemp = $("<p>").html("Temp: " + Math.round(tempArray[i]) + "&#176;F");
+        $(".forecast" + i).append(newTemp);
 
         var newHumidity = $("<p>").html("Humidity: " + humidityArray[i] + "%");
-        $(".forecast" + [i]).append(newHumidity);
+        $(".forecast" + i).append(newHumidity);
+
+        // var card= $(`.forecast${i}`)
+        // card.append()
       }
     });
 }
@@ -122,48 +151,21 @@ function fiveDay(city) {
 // search button click 
 $("#add-city").on("click", function(e) {
   e.preventDefault();
-
   var cityInput = $("#city-input").val().trim();
-  var query = "https://api.openweathermap.org/data/2.5/weather?q=" +
-  cityInput + "&appid=" + apikey;
-
-  $.ajax({
-    url: query,
-    method: "GET",
-  }).then(function (data) {
-    //localCities.push(cityInput);
-    //city = cityInput;
-    $("#city-input, textarea").val(" ");
-    getWeatherDetails();
-  })
+  // var query = "https://api.openweathermap.org/data/2.5/weather?q=" +
+  // cityInput + "&appid=" + apikey;
+  $("#city-input, textarea").val(" ");
+      getWeatherDetails(cityInput);
+  // $.ajax({
+  //   url: query,
+  //   method: "GET",
+  // }).then((city) => {
+  //     //localCities.push(cityInput);
+  //     //city = cityInput;
+  //     $("#city-input, textarea").val(" ");
+  //     getWeatherDetails(city);
+  //     console.log(cityInput);
+  //   })
 })
 
 
-
-
-
-
-// $.ajax({
-//   url: query2,
-//   method: "GET",
-// }).then(function(uvindex) {
-//   //$(".uv-index").text("UV Index: " + weather.main.uvi);
-//   console.log(uvindex);
-// })
-
-//get five day forcast and apply to page
-
-// function getForcast(city) {
-//   var queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" +
-//   city +
-//   "&appid=" +
-//   APIKey;
-
-//   $.ajax({
-//     url: queryURLFive,
-//     method: "GET"
-//   }).then(function(weather) {
-//     $(".forcast").text("City: " + weather.name);
-// });
-
-// console.log(weather);
